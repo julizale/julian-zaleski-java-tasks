f403e10f8a85a91a00d312ba35975ff5b1150a41
Julian Zaleski <julizale@gmail.com>
2022-11-07 20:22:08 +0100

    First commit

---
+++ src/main/java/com/krud/tasks/domain/Task.java
@@ -0,0 +1,13 @@
+package com.krud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Task {
+
+    private Long id;
+    private String title;
+    private String content;
+}
