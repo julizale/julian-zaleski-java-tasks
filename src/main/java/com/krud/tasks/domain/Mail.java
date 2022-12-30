995e7932ee7f855f96965c29b1a5914e1e61e66a
Julian Zaleski <julizale@gmail.com>
2022-12-30 16:28:49 +0100

    23.3 Mail service added

---
+++ src/main/java/com/krud/tasks/domain/Mail.java
@@ -0,0 +1,13 @@
+package com.krud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Mail {
+
+    private String receiverEmail;
+    private String subject;
+    private String message;
+}
---
+++ src/main/java/com/krud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,36 @@
+package com.krud.tasks.service;
+
+import com.krud.tasks.domain.Mail;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SimpleEmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public void send (final Mail mail) {
+        log.info("Preparing to send an email");
+        try {
+            SimpleMailMessage mailMessage = createMessage(mail);
+            javaMailSender.send(mailMessage);
+            log.info("Email successfully sent.");
+        } catch (MailException e) {
+            log.error("Failed to process email sending: " + e.getMessage(), e);
+        }
+    }
+
+    private SimpleMailMessage createMessage(Mail mail) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(mail.getReceiverEmail());
+        simpleMailMessage.setSubject(mail.getSubject());
+        simpleMailMessage.setText(mail.getMessage());
+        return simpleMailMessage;
+    }
+}
